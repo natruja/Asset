66ba36a23f23937a00b6708272f34c48de3af149
Natruja <plelek69@gmail.com>
2017-04-26 09:08:42 +0700

    fixed first page

---
+++ header.php
@@ -0,0 +1,7 @@
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ระบบ ตรวจนับทรัพย์สิน RO10 </title>
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery.min.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Asset RO10</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Asset RO10</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Re-check</a></li>
+          <!--   <li><a href="#about">#</a></li>
+            <li><a href="#contact">#</a></li> -->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
